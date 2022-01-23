55dc9f54c70730d2192c96a77dba898898ae35eb
Juraj Vanco <juraj.vanco@nxp.com>
2022-01-23 23:22:56 +0100

    Rework of the queue component

--- dssim/components/queue.py
+++ dssim/components/queue.py
@@ -14,7 +14,9 @@
 '''
 A queue of events with runtime flexibility of put / get events.
 '''
-from dssim.simulation import DSComponent
+from dssim.simulation import DSComponent, DSSchedulable
+from dssim.pubsub import DSProducer
+
 
 class Queue(DSComponent):
     ''' The (FIFO) queue of events is a SW component which can dynamically
@@ -22,32 +24,78 @@ class Queue(DSComponent):
     a queued event.
     Queue does not use any routing of signals.
     '''
-    def __init__(self, *args, **kwargs):
+    def __init__(self, capacity=float('inf'), *args, **kwargs):
         ''' Init Queue component. No special arguments here. '''
         super().__init__(*args, **kwargs)
+        self.tx_queue_changed = DSProducer(name=self.name+'.tx')
+        self.capacity = capacity
         self.queue = []
-        self.waiting_tasks = []
 
-    def put(self, **event):
-        ''' Put an event into queue. The event can be consumed anytime in the future. '''
-        if not self.waiting_tasks:
-            self.queue.append(event)
-        else:
-            self.sim.signal(self.waiting_tasks[0], **event)
-
-    def get(self, timeout=float('inf')):
-        ''' Get an event from queue. If the queue is empty, wait for the closest event. '''
-        if len(self.queue) > 0:
-            return self.queue.pop(0)
-        self.waiting_tasks.append(self.sim.parent_process)
-        try:
-            obj = yield from self.sim.wait(timeout, cond=lambda c:True)
-        finally:
-            try:
-                waiting_task = self.waiting_tasks.remove(self.sim.parent_process)
-            except ValueError as e:
-                pass
+    def _enqueue(self, obj):
+        self.queue.append(obj)
         return obj
+
+    def _dequeue(self):
+        return self.queue.pop(0)
+
+    def append(self, obj):
+        self.queue.append(obj)
+        self.tx_queue_changed.schedule(0, info='queue changed')
+        return obj
+
+    def pop(self, index):
+        retval = None
+        if len(self.queue) > index:
+            retval = self.queue.pop(index)
+            self.tx_queue_changed.schedule(0, info='queue changed')
+        return retval
+
+    def put_nowait(self, **obj):
+        ''' Put an event into queue. The event can be consumed anytime in the future. '''
+        if len(self) < self.capacity:
+            retval = self.append(obj)  # will emit "queue changed"
+        else:
+            retval = None
+        return retval
+
+    def put(self, timeout=float('inf'), **obj):
+        ''' Put an event into queue. The event can be consumed anytime in the future. '''
+        with self.sim.consume(self.tx_queue_changed):
+            retval = yield from self.sim.check_and_wait(timeout, cond=lambda e:len(self) < self.capacity)  # wait while first element does not match the cond
+        if retval is not None:
+            self.append(obj)  # will emit "queue changed"
+        return retval
+
+    def get_nowait(self, cond=lambda c: True):
+        if len(self) > 0 and cond(self[0]):
+            retval = self.pop(0)  # will emit "queue changed"
+        else:
+            retval = None
+        return retval
+
+    def get(self, timeout=float('inf'), cond=lambda c: True):
+        ''' Get an event from queue. If the queue is empty, wait for the closest event. '''
+        with self.sim.consume(self.tx_queue_changed):
+            retval = yield from self.sim.check_and_wait(timeout, cond=lambda e:len(self) > 0 and cond(self.queue[0]))  # wait while first element does not match the cond
+        if retval is not None:
+            retval = self.pop(0)  # will emit "queue changed"
+        return retval
+
+    def remove(self, cond):
+        ''' Removes event(s) from queue '''
+        # Get list of elements to be removed
+        if len(self.queue) > 0:
+            # Remove all others except the first one
+            self.queue = [e for e in self.queue if not ((callable(cond) and cond(e) or (cond == e)))]
+            # now find what we may emit: "queue changed"
 
     def __len__(self):
         return len(self.queue)
+
+    def __getitem__(self, index):
+        return self.queue[index]
+
+    def __iter__(self):
+        return iter(self.queue)
+
+
