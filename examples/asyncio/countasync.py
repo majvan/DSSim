c8cf5f97b49fd2ac4d50c7d822017e5375b67522
Juraj Vanco <majvan@gmail.com>
2023-03-10 18:22:36 +0100

    Add asyncio examples

---
+++ examples/asyncio/countasync.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# countasync.py
+
+import dssim.parity.asyncio as asyncio
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+    return True
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+    print("Done")
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
