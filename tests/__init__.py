3f4fcfce2bde424c84632a15dcf22905abff6a94
Juraj Vanco <majvan@gmail.com>
2023-04-14 23:12:04 +0200

    Remove unused test files

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,8 @@ from tests.test_interface import TestInterface
 from tests.test_timequeue import TestTimeQueue
 from tests.test_pubsub import TestCallback, TestSubscriber, TestProducer
 from tests.test_simulation import TestDSSchedulable, TestException, TestConditionChecking, TestSim
-from tests.test_queue import TestQueue
-from tests.test_event import TestEvent
+# from tests.test_queue import TestQueue
+# from tests.test_event import TestEvent
 import unittest
 
 if __name__ == '__main__':
