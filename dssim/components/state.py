499cbf6c8ed110f8929a44fef691f10bec4418a4
Juraj Vanco <majvan@gmail.com>
2026-03-04 22:18:24 +0100

    Use sim.schedule rather than tx.schedule

--- dssim/components/state.py
+++ dssim/components/state.py
@@ -37,7 +37,7 @@ class State(DSStatefulComponent):
     def __setitem__(self, key: Any, value: Any) -> bool:
         if (key not in self.state) or self.state[key] != value:
             self.state[key] = value
-            self.tx_changed.schedule_event(0, self.tx_changed)
+            self.sim.schedule_event(0, self.tx_changed, self.tx_changed)
             return True
         return False
 
