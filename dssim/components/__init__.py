358e5a417e9a82e60d3e394781e9554dd17555a9
Juraj Vanco <majvan@gmail.com>
2026-03-10 19:29:38 +0100

    Remake resources

--- dssim/components/__init__.py
+++ dssim/components/__init__.py
@@ -3,6 +3,6 @@ from dssim.components.limiter import Limiter
 from dssim.components.container import Container
 from dssim.base_components import DSQueue, DSLifoQueue, DSKeyQueue
 from dssim.components.queue import Queue
-from dssim.components.resource import Resource, Mutex
+from dssim.components.resource import Resource, PriorityResource, DSResourcePreempted, Mutex
 from dssim.components.state import State
 from dssim.components.timer import Timer
